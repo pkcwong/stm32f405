b79aa96c510f836c041400269d4a5e8c835d716d
Christopher Wong <pkcwong@users.noreply.github.com>
2017-08-06 22:30:10 +0800

    GPIO library

---
+++ config/gpio_config.h
@@ -0,0 +1,23 @@
+#ifndef _GPIO_CONFIG_H
+#define _GPIO_CONFIG_H
+
+#include "gpio.h"
+
+#define IO1 PE0
+#define IO2 PE1
+#define IO3 PE2
+#define IO4 PE3
+#define IO5 PE4
+#define IO6 PE5
+#define IO7 PE6
+#define IO8 PE7
+#define IO9 PE8
+#define IO10 PE9
+#define IO11 PE10
+#define IO12 PE11
+#define IO13 PE12
+#define IO14 PE13
+#define IO15 PE14
+#define IO16 PE15
+
+#endif
---
+++ core/gpio.h
@@ -0,0 +1,24 @@
+#ifndef _GPIO_H
+#define _GPIO_H
+
+#include "stm32f4xx_gpio.h"
+
+class GPIO
+{
+
+	private:
+	GPIO_TypeDef* Port;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	public:
+	GPIO(GPIO_TypeDef* Port, uint16_t Pin);
+	void init(GPIOMode_TypeDef Mode, GPIOOType_TypeDef OType, GPIOPuPd_TypeDef PuPd, GPIOSpeed_TypeDef Speed);
+	void init(GPIOPuPd_TypeDef PuPd);
+	void init(GPIOOType_TypeDef OType, GPIOPuPd_TypeDef PuPd, GPIOSpeed_TypeDef Speed = GPIO_High_Speed);
+	void write(BitAction BitVal);
+	uint8_t read();
+	uint16_t getPinSource();
+
+};
+
+#endif
